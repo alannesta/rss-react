b1de0a02d32f23c1ef900567eeac5818c8e3c7d8
Alan Cao <alannesta@sina.com>
2015-11-06 10:35:18 -0500

    passing onConfirm callback via props

--- public/src/components/feed-modal.js
+++ public/src/components/feed-modal.js
@@ -16,7 +16,7 @@ var FeedModal = React.createClass({
 
 	confirm: function() {
 		console.log(this.props);
-		this.props.onConfirm.apply(null, this.state.modalContent);
+		this.props.onConfirm.call(null, this.state.modalContent);
 		ViewActions.closeModal();
 	},
 
--- public/src/components/view-manager.js
+++ public/src/components/view-manager.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var FeedActions = require('../actions/view-actions');
+var FeedActions = require('../actions/feed-actions');
 var FeedModal = require('./feed-modal');
 
 var ViewStore = require('../stores/view-store');
@@ -9,7 +9,7 @@ var $ = require('jquery');
 var ViewManager = React.createClass({
 
 	regions: {
-		modalRegion: document.getElementsByClassName('modal-container')
+		modalRegion: document.getElementById('modal-container')
 	},
 
 	componentDidMount: function() {
@@ -34,16 +34,17 @@ var ViewManager = React.createClass({
 	_onChange: function() {
 		var manager = this;
 		var viewState = ViewStore.getState();
+
 		// TODO: modal type should also be in view state
 		if (viewState.modalShown) {
 			$('body').addClass('modal-shown');
 			ReactDOM.render(
 				<FeedModal onConfirm = {FeedActions.subscribeFeed} />,
-				document.getElementById('modal-container')
+				manager.regions.modalRegion
 			)
 		}else {
 			$('body').removeClass('modal-shown');
-			ReactDOM.unmountComponentAtNode(document.getElementById('modal-container'));
+			ReactDOM.unmountComponentAtNode(manager.regions.modalRegion);
 		}
 		this.setState(viewState);
 	},
