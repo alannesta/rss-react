46965c471953c4fa6b44506bdc3d5793c889207a
Alan Cao <alannesta@sina.com>
2015-12-18 16:09:39 -0500

    split view-store into different stores

--- public/src/components/view-manager.js
+++ public/src/components/view-manager.js
@@ -8,25 +8,26 @@ var ErrorModal = require('./modals/error-modal');
 var Spinner = require('./spinners/spinner');
 var Toast = require('./toasts/toast');
 
-var ViewStore = require('../stores/view-store');
+var ModalStore = require('../stores/modal-store');
+var ToastStore = require('../stores/toast-store');
 var cacheFactory = require('../utils/state-cache');
 
-var _ = require('underscore');
 var $ = require('jquery');
 
 var ViewManager = React.createClass({
 
 	getInitialState: function() {
-		return ViewStore.getState();
+		return Object.assign({}, {modal: ModalStore.getState()}, {toast: ToastStore.getState()});
 	},
 
 	componentDidMount: function() {
-		cacheFactory.setCache(JSON.stringify(ViewStore.getState()));
-		ViewStore.addChangeListener(this._onChange);
+		ModalStore.addChangeListener(this._onModalChange);
+		ToastStore.addChangeListener(this._onToastChange);
 	},
 
 	componentWillUnmount: function() {
-		ViewStore.removeChangeListener(this._onChange);
+		ModalStore.removeChangeListener(this._onModalChange);
+		ToastStore.removeChangeListener(this._onToastChange);
 	},
 
 	appendModal: function() {
@@ -71,37 +72,40 @@ var ViewManager = React.createClass({
 		}
 	},
 
-	_onChange: function() {
-		console.log('store changed', cacheFactory.getCache());
-		var viewState = ViewStore.getState();
-		this.setState(viewState);
-		if (!cacheFactory.getCache().modal.modalShown && viewState.modal.modalShown) {
+	_onModalChange: function() {
+		console.log('modal changed');
+		var ModalState = ModalStore.getState();
+		if (ModalState.modalShown) {
 			this.appendModal();
-			var modalType = viewState.modal.modalType;
+			var modalType = ModalState.modalType;
 			switch (modalType) {
 				case 'ADD_FEED':
 					ReactDOM.render(
-						<FeedModal content = {viewState.modal.modalContent} onConfirm = {FeedActions.subscribeFeed.bind(FeedActions)} />,
+						<FeedModal content = {ModalState.modalContent} onConfirm = {FeedActions.subscribeFeed.bind(FeedActions)} />,
 						document.getElementById('modal-container')
 					);
 					break;
 				case 'ERROR':
 					ReactDOM.render(
-						<ErrorModal content = {viewState.modal.modalContent} />,
+						<ErrorModal content = {ModalState.modalContent} />,
 						document.getElementById('modal-container')
 					);
 					break;
 			}
-		}else if (cacheFactory.getCache().modal.modalShown) {
+		}else {
 			this.removeModal();
 		}
-		if (!cacheFactory.getCache().toast.toastShown && viewState.toast.toastShown) {
+	},
+
+	_onToastChange: function() {
+		console.log('toast changed');
+		var ToastState = ToastStore.getState();
+		this.setState(ToastState);
+		if (ToastState.toastShown) {
 			this.showToast();
-		}else if (cacheFactory.getCache().toast.toastShown && !viewState.toast.toastShown){
-			console.log('remove');
+		}else{
 			this.removeToast();
 		}
-		cacheFactory.setCache(JSON.stringify(viewState));
 	},
 
 	render: function() {
---
+++ public/src/stores/modal-store.js
@@ -0,0 +1,47 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE_EVENT = 'change';
+
+var _modalState = {
+	modalShown: false,
+	modalType: '',
+	modalContent: {}
+};
+
+var ModalStore = Object.assign({}, EventEmitter.prototype, {
+	getState: function() {
+		return _modalState;
+	},
+
+	emitChange: function() {
+		this.emit(CHANGE_EVENT);
+	},
+
+	addChangeListener: function(callback) {
+		this.on(CHANGE_EVENT, callback);
+	},
+
+	removeChangeListener: function(callback) {
+		this.removeListener(CHANGE_EVENT, callback);
+	}
+});
+
+
+AppDispatcher.register(function(action) {
+	switch(action.actionType) {
+		case 'SHOW_MODAL':
+			_modalState.modalShown = true;
+			_modalState.modalType = action.modalType;
+			_modalState.modalContent = action.content;
+			ModalStore.emitChange();
+			break;
+
+		case 'CLOSE_MODAL':
+			_modalState.modalShown = false;
+			ModalStore.emitChange();
+			break;
+	}
+});
+
+module.exports = ModalStore;
